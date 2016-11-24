60366557cac200692977fb390a04e01b00357c87
chenlong1 <chenlong1@liandisys.com.cn>
2016-11-24 17:29:00 +0800

    rails study on 《agile web development of rails》stopped

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ApplicationRecord
+  validates :title,:image_url,:description,:price ,:presence=>{message:"不能为空哦！！"}#:??=> 和??:都行
+  validates :price,:numericality=>{:greater_than_or_equal_to => 0.01,message:"开价为负，马云也输"}
+  validates :title ,:uniqueness=>{message:"商品名冲突"}
+  validates :image_url,format:{
+      with:/\.(gif|jpg|png)$/i,
+      message:'图片格式错误，只能是gif,jpg,png格式',
+      :multiline => true
+  }
 end
